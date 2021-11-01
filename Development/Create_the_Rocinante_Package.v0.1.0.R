3d24afd1ae6c2944c914d3598870afa3c78975de
vertesy <abel.vertesy@imba.oeaw.ac.at>
2021-11-01 09:44:10 +0100

    Dev folder

---
+++ Development/Create_the_Rocinante_Package.v0.1.0.R
@@ -0,0 +1,119 @@
+######################################################################################################
+# Create_the_Rocinante_Package.v0.1.R
+# 31 10 2021
+######################################################################################################
+# source("/Users/abel.vertesy/GitHub/Packages/Rocinante/Development/Create_the_Rocinante_Package.v0.1.R")
+rm(list = ls(all.names = TRUE));
+try(dev.off(), silent = TRUE)
+# install.packages("devtools")
+# Functions ------------------------
+try (source('~/GitHub/Packages/CodeAndRoll/CodeAndRoll.R'),silent= FALSE)
+
+# irequire("devtools")
+# install_version("devtools", version = "2.0.2", repos = "http://cran.at.r-project.org")
+irequire("devtools")
+irequire("roxygen2")
+irequire("stringr")
+
+kollapse <-function(..., print = TRUE) {
+if (print == TRUE) {
+    print(paste0(c(...), collapse = ""))
+  }
+  paste0(c(...), collapse = "")
+}
+
+# Setup ------------------------
+PackageName = 	"Rocinante"
+setwd("~/GitHub/Packages/")
+
+RepositoryDir = kollapse("~/GitHub/Packages/", PackageName, "/")
+fname = 	kollapse(PackageName, ".R")
+Package_FnP = 	kollapse(RepositoryDir, "R/", fname)
+
+BackupDir = "~/GitHub/Packages/Rocinante/Development/"
+dir.create(BackupDir)
+
+# devtools::use_package("vioplot")
+DESCRIPTION <- list("Title" = "Rocinante is the fastest way to create, annotate and export plots in R"
+    , "Author" = person(given = "Abel", family = "Vertesy", email = "abel.vertesy@imba.oeaw.ac.at", role =  c("aut", "cre") )
+    , "Authors@R" = 'person(given = "Abel", family = "Vertesy", email = "a.vertesy@imba.oeaw.ac.at", role =  c("aut", "cre") )'
+    , "Description" = "Rocinante is a set of R functions that allows you to generate precise figures easily,
+    and create clean markdown reports about what you just discovered with your analysis script. It is ggplot-based smaller brother of vertesy/MarkdownReports. It helps you to:
+    1. Create scientifically accurate (annotated) figures with very short code, making use of variable-, row- and columnnames.
+    2. Save figures automatically as vector graphic (.pdf), that you can use from presentation to posters anywhere.
+    3. Incorporate your figures automatically in a markdown report file.
+    4. Describe your figures & findings in the same report in a clear and nicely formatted way, parsed from your variables into english sentences.
+    5. Share your report, by exporting your report to .pdf, .html or .docx, or via Github or a personal website."
+    , "License" = "GPL-3 + file LICENSE"
+    , "Version" = "0.1.0"
+    # , "Version" = "4.0.0"
+    , "Packaged" =  Sys.time()
+    , "Repository" =  "CRAN"
+    , "Imports" = "tidyverse, cowplot, ggpubr, stats, methods, sm, graphics, grDevices, gplots, RColorBrewer, sessioninfo, MarkdownReports"
+    # , "Suggests" = ""
+    , "BugReports"= "https://github.com/vertesy/Rocinante/issues"
+)
+
+
+setwd(RepositoryDir)
+if ( !dir.exists(RepositoryDir) ) { create(path = RepositoryDir, description = DESCRIPTION, rstudio = TRUE)
+} else {
+    getwd()
+    try(file.remove(c("DESCRIPTION","NAMESPACE", "Rocinante.Rproj")))
+    create_package(path = RepositoryDir, fields = DESCRIPTION)
+}
+
+
+# go and write fun's ------------------------------------------------------------------------
+# file.edit(Package_FnP)
+
+# Create Roxygen Skeletons ------------------------
+# RoxygenReady(Package_FnP)
+
+# replace output files ------------------------------------------------
+BackupOldFile = 	kollapse(BackupDir, "Development", ".bac", print = FALSE)
+AnnotatedFile = 	kollapse(BackupDir, "Development", ".annot.R", print = FALSE)
+file.copy(from = Package_FnP, to = BackupOldFile, overwrite = TRUE)
+# file.copy(from = AnnotatedFile, to = Package_FnP, overwrite = TRUE)
+
+# Manual editing of descriptors ------------------------------------------------
+# file.edit(Package_FnP)
+
+# Compile a package ------------------------------------------------
+setwd(RepositoryDir)
+getwd()
+document()
+
+
+# Install your package ------------------------------------------------
+# # setwd(RepositoryDir)
+install(RepositoryDir)
+# require("Rocinante")
+# # remove.packages("Rocinante")
+# # Test your package ------------------------------------------------
+# help("wplot")
+# cat("\014")
+# devtools::run_examples()
+
+
+# Test if you can install from github ------------------------------------------------
+# devtools::install_github(repo = "vertesy/Rocinante")
+
+# require("Rocinante")
+
+# Clean up if not needed anymore ------------------------------------------------
+# View(installed.packages())
+# remove.packages("Rocinante")
+
+check(RepositoryDir, cran = TRUE)
+# as.package(RepositoryDir)
+#
+#
+# # source("https://install-github.me/r-lib/desc")
+# # library(desc)
+# # desc$set("Rocinante", "foo")
+# # desc$get(Rocinante)
+#
+#
+# system("cd ~/GitHub/Rocinante/; ls -a; open .Rbuildignore")
+#
