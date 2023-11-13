cbde2e590326e9ac9845ed48ee92a58aaf659865
vertesy <abel.vertesy@imba.oeaw.ac.at>
2023-11-13 13:33:38 +0100

    Update Create_the_Rocinante_Package.v0.1.0.R

--- Development/Create_the_Rocinante_Package.v0.1.0.R
+++ Development/Create_the_Rocinante_Package.v0.1.0.R
@@ -1,50 +1,37 @@
 ######################################################################################################
-# Create_the_Rocinante_Package.v0.1.R
-# 31 10 2021
+# Create_the_Rocinante_Package.R
 ######################################################################################################
-# source("/Users/abel.vertesy/GitHub/Packages/Rocinante/Development/Create_the_Rocinante_Package.v0.1.R")
+# source("/Users/abel.vertesy/GitHub/Packages/Rocinante/Development/Create_the_Rocinante_Package.R")
 rm(list = ls(all.names = TRUE));
 try(dev.off(), silent = TRUE)
 # install.packages("devtools")
 # Functions ------------------------
-try (source('~/GitHub/Packages/CodeAndRoll/CodeAndRoll.R'),silent= FALSE)
+# try (source('~/GitHub/Packages/CodeAndRoll/CodeAndRoll.R'),silent= FALSE)
 
+# # irequire("devtools")
+# # install_version("devtools", version = "2.0.2", repos = "http://cran.at.r-project.org")
 # irequire("devtools")
-# install_version("devtools", version = "2.0.2", repos = "http://cran.at.r-project.org")
-irequire("devtools")
-irequire("roxygen2")
-irequire("stringr")
+# irequire("roxygen2")
+# irequire("stringr")
 
-kollapse <-function(..., print = TRUE) {
-if (print == TRUE) {
-    print(paste0(c(...), collapse = ""))
-  }
-  paste0(c(...), collapse = "")
-}
 
 # Setup ------------------------
-PackageName = 	"Rocinante"
-package.version =  "0.1.0"
+package.name <- 	"Rocinante"
+package.version <- "0.1.0"
 setwd("~/GitHub/Packages/")
 
-RepositoryDir = kollapse("~/GitHub/Packages/", PackageName, "/")
-fname = 	kollapse(PackageName, ".R")
-Package_FnP = 	kollapse(RepositoryDir, "R/", fname)
+RepositoryDir <- paste0("~/GitHub/Packages/", PackageName, "/")
+fname <-	paste0(PackageName, ".R")
+Package_FnP <-		paste0(RepositoryDir, "R/", fname)
 
-BackupDir = "~/GitHub/Packages/Rocinante/Development/"
+BackupDir <- "~/GitHub/Packages/Rocinante/Development/"
 dir.create(BackupDir)
 
 # devtools::use_package("vioplot")
-DESCRIPTION <- list("Title" = "Rocinante is the fastest way to create, annotate and export plots in R"
+DESCRIPTION <- list("Title" = "Rocinante is a set of helper functions."
     , "Author" = person(given = "Abel", family = "Vertesy", email = "abel.vertesy@imba.oeaw.ac.at", role =  c("aut", "cre") )
     , "Authors@R" = 'person(given = "Abel", family = "Vertesy", email = "a.vertesy@imba.oeaw.ac.at", role =  c("aut", "cre") )'
-    , "Description" = "Rocinante is a set of R functions that allows you to generate precise figures easily,
-    and create clean markdown reports about what you just discovered with your analysis script. It is ggplot-based smaller brother of vertesy/MarkdownReports. It helps you to:
-    1. Create scientifically accurate (annotated) figures with very short code, making use of variable-, row- and columnnames.
-    2. Save figures automatically as vector graphic (.pdf), that you can use from presentation to posters anywhere.
-    3. Incorporate your figures automatically in a markdown report file.
-    4. Describe your figures & findings in the same report in a clear and nicely formatted way, parsed from your variables into english sentences.
-    5. Share your report, by exporting your report to .pdf, .html or .docx, or via Github or a personal website."
+    , "Description" = "Rocinante is a set of R functions to help coding in R."
     , "License" = "GPL-3 + file LICENSE"
     , "Version" = package.version
     # , "Version" = "4.0.0"
@@ -72,8 +59,8 @@ if ( !dir.exists(RepositoryDir) ) { create(path = RepositoryDir, description = D
 # RoxygenReady(Package_FnP)
 
 # replace output files ------------------------------------------------
-BackupOldFile = 	kollapse(BackupDir, "Development", ".bac", print = FALSE)
-AnnotatedFile = 	kollapse(BackupDir, "Development", ".annot.R", print = FALSE)
+BackupOldFile <-	paste0(BackupDir, "Development", ".bac", print = FALSE)
+AnnotatedFile <-	paste0(BackupDir, "Development", ".annot.R", print = FALSE)
 file.copy(from = Package_FnP, to = BackupOldFile, overwrite = TRUE)
 # file.copy(from = AnnotatedFile, to = Package_FnP, overwrite = TRUE)
 
@@ -83,12 +70,12 @@ file.copy(from = Package_FnP, to = BackupOldFile, overwrite = TRUE)
 # Compile a package ------------------------------------------------
 setwd(RepositoryDir)
 getwd()
-document()
+devtools::document()
 
 
 # Install your package ------------------------------------------------
 # # setwd(RepositoryDir)
-install(RepositoryDir)
+devtools::install(RepositoryDir)
 # require("Rocinante")
 # # remove.packages("Rocinante")
 # # Test your package ------------------------------------------------
