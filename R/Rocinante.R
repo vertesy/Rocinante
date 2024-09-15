6a13054d0d8122e5122b8c6bdd76f310f81fc450
vertesy <abel.vertesy@imba.oeaw.ac.at>
2024-09-15 16:50:04 +0200

    ...

--- R/Rocinante.R
+++ R/Rocinante.R
@@ -324,7 +324,7 @@ memory.biggest.objects <- function(n = 5, plot = T, saveplot = FALSE) {
   # strX <- as.character(capture.output(dput(top.names)))
   strX <- kollapse(top.names, collapseby = "', '")
   # strX <- gsub('[^A-Za-z0-9 ,._/()]', '', strX)
-  message("rm(list = ('", strX, "'))\n")
+  message("rm(list = c('", strX, "'))\n")
 
   if(plot) {
     pie(x = Memorty.usage.stat, cex = .5, sub = date(),
